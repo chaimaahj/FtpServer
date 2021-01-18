031af4fadd40ea4d34ac1ca301aae4937eeb4eb5
chaimaahj <49591407+chaimaahj@users.noreply.github.com>
2021-01-18 15:29:09 +0100

    Add files via upload

--- src/Server/FtpServer.java
+++ src/Server/FtpServer.java
@@ -1,19 +1,33 @@
 package Server;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.ServerSocket;
 
 public class FtpServer {
 	
 	private ServerSocket firstSocket;
-	private int port = 1011;
+	private int port = 1075;
+	OutputStream out;
 	
 	public FtpServer()
 	{
 		try{
 			firstSocket = new ServerSocket(port);
-			Socket clt = firstSocket.accept();
+			Socket con = firstSocket.accept();
+			out=con.getOutputStream() ;
+			DataOutputStream dos = new DataOutputStream(out);
+			dos.writeBytes("220 Service ready\r\n");
+			InputStream is = con.getInputStream(); 
+			InputStreamReader isr = new InputStreamReader(is);
+	        BufferedReader br = new BufferedReader(isr);
+	        String str = br.readLine();
+			
 		}catch(IOException e)
 		{
 			e.printStackTrace();
@@ -22,7 +36,7 @@ public class FtpServer {
 	}
 
 	public static void main(String[] args) {
-		
+		new FtpServer();
 
 	}
 
