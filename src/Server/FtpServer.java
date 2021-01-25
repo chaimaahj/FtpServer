f54be120de9be10ab88330babbf4d9af0d13e3bc
chaimaahj <49591407+chaimaahj@users.noreply.github.com>
2021-01-25 15:33:45 +0100

    Add files via upload

--- src/Server/FtpServer.java
+++ src/Server/FtpServer.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.ServerSocket;
@@ -25,10 +26,8 @@ public class FtpServer {
 	private int port = 1076;
 	private OutputStream out;
 	private Socket con;
-	public Status status = Status.NOT_LOGGED;
-	public enum Status {
-		NOT_LOGGED, ENTERED_LOGIN, LOGGED
-	}
+	
+	
 	
 	
 	public FtpServer()
@@ -44,87 +43,27 @@ public class FtpServer {
 			InputStreamReader isr = new InputStreamReader(is);
 	         br = new BufferedReader(isr);
 	        String str = br.readLine();
-	        
 	        InputStream in = con.getInputStream();
 	        DataInputStream dis= new DataInputStream(in);
-	       
+	        while(true)
+	        {
+	        	if(str.equals("chaimaa"))
+	        	{
+	        		dos.writeBytes("331 Username OK\r\n");
+	        	}
+	        	if(str.equals("azerty"))
+	        	{
+	        		dos.writeBytes("230 User logged in\r\n");
+	        	}
+	        }
 		
 		}catch(IOException e){
 		e.printStackTrace();
 					}
 	}
 	
-	public void executeCommand(String c) throws IOException {
-		int index = c.indexOf(" ");
-		String command = ((index == -1) ? c.toUpperCase() : c.substring(0, index).toUpperCase());
-		String args = (( index == -1) ? null : c.substring(index+1, c.length()));
-		
-		System.out.println("Command: "+command + " Args: "+ args);
-		
-		switch(command) {
-			case "USER": 
-				handleUSER(args);
-				break;
-			case "PASS": 
-				handlePASS(args);
-				break;
-			case "QUIT":
-				handleQUIT();
-				break;
-			default: 
-				System.out.println("501 Unkown command");
-				break;
-		}
-	}
 	
-	private void handleUSER(String username) throws IOException {
-		
-		if(username.toLowerCase().equals("chaimaa")) {
-			DataOutputStream dos = new DataOutputStream(out);
-			dos.writeBytes("331 Username ok\r\n");
-		    this.status = Status.ENTERED_LOGIN;
-		}
-		else if(this.status == Status.LOGGED) {
-			DataOutputStream dos = new DataOutputStream(out);
-			dos.writeBytes("User already logged in\r\n");
-		}
-		else {
-			DataOutputStream dos = new DataOutputStream(out);
-			dos.writeBytes("USER Not logged in\r\n");
-		
-		}
-	}
 	
-	public void handlePASS(String password) throws IOException {
-		if(this.status  == Status.ENTERED_LOGIN && password.equals("1234")) {
-			this.status = Status.LOGGED;
-			DataOutputStream dos = new DataOutputStream(out);
-			dos.writeBytes("230 User logged in\r\n");
-			
-		}
-		else if(this.status == Status.LOGGED) {
-			DataOutputStream dos = new DataOutputStream(out);
-			dos.writeBytes(" User already logged in\r\n");
-		}
-		else {
-			DataOutputStream dos = new DataOutputStream(out);
-			dos.writeBytes("USER Not logged in\r\n");
-		}
-	}
-	
-	private void handleQUIT() throws IOException {
-		try {
-			br.close();
-			this.p.close();
-			this.firstSocket.close();
-			DataOutputStream dos = new DataOutputStream(out);
-			dos.writeBytes("disconnecting from SERVER\r\n");
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-}
-
 		
 	}
 	
